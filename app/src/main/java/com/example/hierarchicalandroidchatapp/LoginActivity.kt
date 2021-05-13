d8300ed47935b28eed0da4ee902a45c742ace9cf
Jason <c14170129@john.petra.ac.id>
2021-05-14 04:08:41 +0700

    "Try Out Login"

--- app/src/main/java/com/example/hierarchicalandroidchatapp/LoginActivity.kt
+++ app/src/main/java/com/example/hierarchicalandroidchatapp/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.example.hierarchicalandroidchatapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
@@ -14,21 +16,31 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        emailObj = findViewById<EditText>(R.id.editTextTextEmailLogAct)
-        passwordObj = findViewById<EditText>(R.id.editTextTextPasswordLogAct)
-
         val loginButtonLogAct = findViewById<Button>(R.id.loginButtonLogAct)
 
         loginButtonLogAct.setOnClickListener {
-            val email = emailObj.text.toString()
-            val password = passwordObj.text.toString()
-            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener {
+            performLogin()
+        }
+    }
 
-            }
+    private fun performLogin() {
+        emailObj = findViewById<EditText>(R.id.editTextTextEmailLogAct)
+        passwordObj = findViewById<EditText>(R.id.editTextTextPasswordLogAct)
+
+        val email = emailObj.text.toString()
+        val password = passwordObj.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill out email and password", Toast.LENGTH_SHORT).show()
+            return
         }
 
-        loginButtonLogAct.setOnClickListener {
-            finish()
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener {
+            if (!it.isSuccessful) return@addOnCompleteListener
+
+            val intent = Intent(this, LatestMessagesActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
         }
     }
 }
